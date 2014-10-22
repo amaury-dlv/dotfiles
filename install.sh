44a9607c928709f5770b60af30c8431540489925
Amaury de la Vieuville <amaury.dlv@gmail.com>
2014-10-22 10:00:43 +0200

    Remove breaking keyboard mapping

--- install.sh
+++ install.sh
@@ -8,13 +8,3 @@ done
 
 echo "Installing vim bundles"
 vim +BundleInstall +qall
-
-echo "Remap Caps Lock to Ctrl"
-
-if [ "$(uname)" = "Darwin" ];
-then
-  vendor=$(echo $(ioreg -n IOHIDKeyboard -r | grep -e VendorID\" | cut -d'=' -f2))
-  product=$(echo $(ioreg -n IOHIDKeyboard -r | grep -e ProductID\" | cut -d'=' -f2))
-
-  defaults -currentHost write NSGlobalDomain com.apple.keyboard.modifiermapping.${vendor}-${product}-0 -array '<array><dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict></array>'
-fi
