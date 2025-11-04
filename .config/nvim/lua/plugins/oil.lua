adf50be068ae3cc9259e221c43aaad9063873399
Amaury de la Vieuville <adelavieuville@apple.com>
2025-11-04 09:55:01 +0100

    nvim: oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,7 @@ return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local detail = false
     require("oil").setup({
       default_file_explorer = true,
       columns = {
@@ -49,6 +50,17 @@ return {
         ["gx"] = "actions.open_external",
         ["g."] = { "actions.toggle_hidden", mode = "n" },
         ["g\\"] = { "actions.toggle_trash", mode = "n" },
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
       },
       use_default_keymaps = true,
       view_options = {
